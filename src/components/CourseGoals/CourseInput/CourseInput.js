381ea95eec180573724f4fb80455be4c5461abea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-07 11:07:16 +0300

    Defined the logic to setIsValid to true

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,10 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
@@ -18,8 +22,6 @@ const CourseInput = (props) => {
       setIsValid(false);
       return;
     }
-
-    setIsValid(true);
 
     props.onAddGoal(enteredValue);
   };
