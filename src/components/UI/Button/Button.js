3e4edd1042733c07a79019b65982c82806a4aa8c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-07 11:22:36 +0300

    A few changes were made

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-import "./Button.css";
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button``;
 
 export default Button;
